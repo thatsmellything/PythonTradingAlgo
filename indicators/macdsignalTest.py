47ee17e9c2a1f63c01d61859831ce99c5958dba2
Jensen Judkins <thesmelliestman@gmail.com>
2021-12-10 08:47:20 -0700

    can grab stock data info and now use the info grabber for indicators

--- indicators/macdsignalTest.py
+++ indicators/macdsignalTest.py
@@ -1,7 +1,13 @@
 import btalib
 import pandas as pd
 
-df = pd.read_csv("data/btalibtest.txt", parse_dates=True, index_col='Date')
+df = pd.read_csv("../data/btalibtest.txt", parse_dates=True, index_col='Date')
+
+macd = btalib.macd(df)
+
+print(macd.df) 
+
+df = pd.read_csv("../dataPull/stockdata/AAPL.txt", parse_dates=True, index_col='Date')
 
 macd = btalib.macd(df)
 
