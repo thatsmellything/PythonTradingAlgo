5eea1bdac740404e4e717830aaba67fc1453cbe1
Jensen Judkins <thesmelliestman@gmail.com>
2021-12-09 23:42:25 -0700

    added documentation

--- indicators/macdsignalTest.py
+++ indicators/macdsignalTest.py
@@ -3,6 +3,6 @@ import pandas as pd
 
 df = pd.read_csv("data/btalibtest.txt", parse_dates=True, index_col='Date')
 
-macd = btalib.macd(df) #5 day moving average with data supplied from csv file
+macd = btalib.macd(df)
 
 print(macd.df) 
