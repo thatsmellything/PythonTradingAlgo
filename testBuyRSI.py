46d36f2d5ade57d79262df5e193cd4d92d204851
Jensen Judkins <thesmelliestman@gmail.com>
2021-12-11 20:28:13 -0700

    Update testBuyRSI.py

--- testBuyRSI.py
+++ testBuyRSI.py
@@ -56,4 +56,4 @@ def makelimitorderiflowrsi(timeframe, symbol, datalimit, amount, lowerthanrsi):
         print("buying {} of {}, type {} {} {} at {}".format(amount, symbol, 'limit', 'buy', 'opg', price))
     else:
         print("not buying any stocks")
-makelimitorderiflowrsi('1D', 'CLSK', '100', '100', '35')
+#makelimitorderiflowrsi('1D', 'CLSK', '100', '100', '35')
