d7859c10e96e6d2491c0e4bd0a106ebefa662af0
thatsmellything <thesmelliestman@gmail.com>
2021-12-13 12:56:40 -0700

    made buy and sell test

--- buyandsellonrsi.py
+++ buyandsellonrsi.py
@@ -1,6 +1,7 @@
 import requests, json
 from config import *
 import pullandprocess
+from time import time, sleep
 
 BASE_URL = "https://paper-api.alpaca.markets"
 ACCOUNT_URL = "{}/v2/account".format(BASE_URL)
@@ -48,6 +49,21 @@ def makemarketorderiflowrsi(timeframe, symbol, datalimit, amount, lowerthanrsi):
 #makemarketorderiflowrsi('1D', 'CLSK', '1000', '100', '45')
 
 
+def makelimitorderiflowrsi(timeframeofdata, symbol, datalimit, amount, lowerthanrsi, ordergoodfor):
+    if float(pullandprocess.returnLatestRsi(timeframeofdata, symbol, datalimit).strip("\n")) < float(lowerthanrsi):
+        price = pullandprocess.returnClosingVal(timeframeofdata, symbol, datalimit).strip("\n")
+        #print(price)
+        create_order_limit(symbol, amount, 'buy', 'limit', 'gtc', '{}'.format(price))
+        print("buying {} of {}, type {} {} {} at {}".format(amount, symbol, 'limit', 'buy', ordergoodfor, price))
+        sleep(360)
+        makeLimitOrderIfRSIUp('1min', 'CLSK', '1000', '100', '45', 'day')
+    else:
+        print("not buying any stocks")
+
+
+
+
+
 def makeLimitOrderIfRSIUp(timeframeofdata, symbol, datalimit, amount, lowerthanrsi, ordergoodfor):
     if float(pullandprocess.returnLatestRsi(timeframeofdata, symbol, datalimit).strip("\n")) > float(lowerthanrsi):
         print("RSI value has been met, selling at yesterdays close price")
@@ -62,3 +78,9 @@ def makeLimitOrderIfRSIUp(timeframeofdata, symbol, datalimit, amount, lowerthanr
         create_order_limit(symbol, amount, 'sell', 'limit', 'day', '{}'.format(priceToSell))
         print("Selling {} of {}, type {} {} {} at {}".format(amount, symbol, 'limit', 'sell', ordergoodfor, priceToSell))
 #makeLimitOrderIfRSIUp('1D', 'CLSK', '1000', '100', '45', 'day')
+
+while True:
+    sleep(60 - time() % 60)
+	# thing to run
+    makelimitorderiflowrsi('1min', 'CLSK', '1000', '100', '35', 'day')
+
