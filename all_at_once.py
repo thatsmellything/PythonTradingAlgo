dff17ff9ca89e5f0d06491011a85a8ea8e720c94
Jensen Judkins <thesmelliestman@gmail.com>
2021-12-15 16:52:45 -0700

    Update all_at_once.py

--- all_at_once.py
+++ all_at_once.py
@@ -3,6 +3,8 @@ import requests, json
 from datetime import datetime
 import alpaca_trade_api as tradeapi
 from config import *
+import btalib
+import pandas as pd
 
 ###FIRST PRINT ACCOUNT INFO TO MAKE SURE WE HAVE A CONNECTION###
 def print_account_information():
@@ -56,10 +58,10 @@ def pull_data(timeframe, symbols, limit):
             volume = bar['v']
             f.write('{},{},{},{},{},{}\n'.format(day, open_price, high_price, low_price, close_price, volume))
 
-pull_data('1D','SPY','100')
-pull_data('1Min', 'TR', '1000')
+#pull_data('1D','SPY','100')
+#pull_data('1Min', 'TR', '1000')
 
-###FORMAT DATA .txt INTO .csv###
+###FORMAT DATA .txt INTO .csv (turns out not needed)###
 def format_data(symbol):
     filename = 'data/{}.txt'.format(symbol)
     f = open(filename, 'r')
@@ -71,5 +73,164 @@ def format_data(symbol):
     for line in lines:
         f.write(line.replace(',', ' '))
     f.close()
-format_data('SPY')
+#format_data('SPY')
+
 #NEED TO ADD MACD AND RSI TO CSV
+def get_indicators(symbol):
+    df = pd.read_csv("data/{}.txt".format(symbol), parse_dates=True, index_col=0)
+    #print(df)
+    sma = btalib.sma(df)
+    rsi = btalib.rsi(df)
+    #macd = btalib.macd(df)
+    
+    
+    df['sma'] = sma.df
+    df['rsi'] = rsi.df
+    #df['macd'] = macd.df
+    
+    print(df)
+    df.to_csv('data/{}.csv'.format(symbol), index=True)
+
+    ##THESE DO NOT WORK FOR NOW##
+    #macd = btalib.macd(df)
+    #print(macd)
+    #rsi = btalib.rsi(df) #period default is 14, rsi = btalib.rsi(df, period = 10)
+    #signal = btalib.macd_signal(df)
+    #histogram = btalib.macd_histogram(df)
+    
+    #df['macd'] = macd.df
+    #df['signal'] = signal.df
+    #df['histogram'] = histogram.df
+    #df['rsi'] = rsi.df
+
+
+    return df
+get_indicators('SPY')
+
+###GRAB THE LAST ENTRY OF THE CSV FILE###
+def get_last_entry(symbol):
+    with open("data/" + symbol + ".csv", 'r') as file:
+        data = file.readlines() 
+    lastRow = data[-1]
+    return lastRow
+#print(get_last_entry('SPY'))
+
+###RETURN LATEST INDICATORS###
+
+###GET RSI###
+def get_rsi(symbol):
+    date, open_value, high_value, low_value, closing_value, volume, sma, rsi  = get_last_entry(symbol).split(',')
+    #print(rsi)
+    return rsi
+get_rsi('SPY')
+###GET Closing Value###
+def get_closing_value(symbol):
+    date, open_value, high_value, low_value, closing_value, volume, sma, rsi  = get_last_entry(symbol).split(',')
+    #print(closing_value)
+    return closing_value
+get_closing_value('SPY')
+###GET SMA###
+def get_sma(symbol):
+    date, open_value, high_value, low_value, closing_value, volume, sma, rsi  = get_last_entry(symbol).split(',')
+    #print(sma)
+    return sma
+get_sma('SPY')
+###GET Volume###
+def get_volume(symbol):
+    date, open_value, high_value, low_value, closing_value, volume, sma, rsi  = get_last_entry(symbol).split(',')
+    #print(volume)
+    return volume
+get_volume('SPY')
+###GET Open Value###
+def get_open_value(symbol):
+    date, open_value, high_value, low_value, closing_value, volume, sma, rsi  = get_last_entry(symbol).split(',')
+    #print(open_value)
+    return open_value
+get_open_value('SPY')
+###GET High Value###
+def get_high_value(symbol):
+    date, open_value, high_value, low_value, closing_value, volume, sma, rsi  = get_last_entry(symbol).split(',')
+    #print(high_value)
+    return high_value
+get_high_value('SPY')
+###GET Low Value###
+def get_low_value(symbol):
+    date, open_value, high_value, low_value, closing_value, volume, sma, rsi  = get_last_entry(symbol).split(',')
+    #print(low_value)
+    return low_value
+get_low_value('SPY')
+###GET Date###
+def get_date(symbol):
+    date, open_value, high_value, low_value, closing_value, volume, sma, rsi  = get_last_entry(symbol).split(',')
+    #print(date)
+    return date
+get_date('SPY')
+
+
+
+
+###CREATE A LIMIT ORDER###
+def create_limit_order(symbol, qty, side, type, time_in_force, limit_price):
+    BASE_URL = "https://paper-api.alpaca.markets"
+    ACCOUNT_URL = "{}/v2/account".format(BASE_URL)
+    ORDERS_URL = "{}/v2/orders".format(BASE_URL)
+    HEADERS = {'APCA-API-KEY-ID': API_KEY_ID, 'APCA-API-SECRET-KEY': SECRET_KEY}
+    data = {
+        "symbol":symbol,
+        "qty":qty,
+        "side":side,
+        "type":type,
+        "time_in_force":time_in_force,
+        "limit_price":limit_price
+    }
+    r = requests.post(ORDERS_URL, json=data, headers=HEADERS)
+
+    return json.loads(r.content)
+
+###CHECK RSI LEVELS AND SELL ORDER IF ABOVE THRESHOLD###
+def make_limit_sell_order_if_rsi_high(symbol, amount, rsi_value, profit_percentage):
+    order_good_for = 'gtc'
+    print('Checking if RSI is higher than {}'.format(rsi_value))
+    if float(get_rsi(symbol).strip("\n")) > float(rsi_value):
+        print("RSI value has been met, selling at yesterdays close price")
+        price_at_close = get_closing_value.strip("\n")
+        create_limit_order(symbol, amount, 'sell', 'limit', 'gtc', '{}'.format(price_at_close))
+        print("Selling {} of {}, type {} {} {} at {}".format(amount, symbol, 'limit', 'sell', order_good_for, price_at_close))
+    else:
+        print("RSI value to sell has not been met, taking {}% profit for the day".format(profit_percentage))
+        price_at_close = get_closing_value(symbol).strip("\n")
+        price_to_sell = float(price_at_close) * float(profit_percentage)
+        #print(price)
+        create_limit_order(symbol, amount, 'sell', 'limit', 'gtc', '{}'.format(price_to_sell))
+        print("Selling {} of {}, type {} {} {} at {}".format(amount, symbol, 'limit', 'sell', order_good_for, price_to_sell))
+#makeLimitOrderIfRSIUp('1D', 'CLSK', '1000', '100', '45', 'day')
+
+###CHECK RSI LEVELS AND BUY ORDER IF BELOW THRESHOLD###
+def make_limit_buy_order_if_low_rsi(symbol, amount, rsi_value):
+    order_good_for = 'day'
+    #rsivalueString = (pullandprocess.returnLatestRsi(timeframeofdata, symbol, datalimit))
+    #rsivalue = float(rsivalueString)
+    #print("attempting to buy {} {} stock, and gain at least {} percent, rsi value is ".format(amount, symbol, profitpercentage))
+
+    #print("attempting to buy {} {} stock, and gain at least {} percent".format(amount, symbol, profitpercentage))
+    print('Checking if RSI is lower than {}'.format(rsi_value))
+    if float(get_rsi(symbol).strip("\n")) < float(rsi_value):
+        price = get_closing_value(symbol).strip("\n")
+        print(price)
+        create_limit_order(symbol, amount, 'buy', 'limit', order_good_for, '{}'.format(price))
+        
+        print("Buying {} of {}, type {} {} {} at {}".format(amount, symbol, 'limit', 'buy', order_good_for, price))
+        profit_percentage = 1.06
+        make_limit_sell_order_if_rsi_high(symbol, amount, '{}'.format(float(rsi_value) +10), '{}'.format(profit_percentage))
+
+       
+    else:
+
+        print("RSI value is not below {}, not buying any stocks".format(rsi_value))
+#make_limit_buy_order_if_low_rsi('SPY', 1, '35')
+
+
+
+
+def start_program():
+    print('test')
