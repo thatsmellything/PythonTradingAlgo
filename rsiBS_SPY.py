a1e727d4b0659750fe5d1ebed4604143b7051d67
Jensen Judkins <thesmelliestman@gmail.com>
2021-12-14 21:48:04 -0700

    Revert "Update rsiBS_SPY.py"
    This reverts commit 1bc8c9c0f97127a7b7a47e4e1cc2113fbb85b654.

--- rsiBS_SPY.py
+++ rsiBS_SPY.py
@@ -34,7 +34,13 @@ def create_order_limit(symbol, qty, side, type, time_in_force,limit_price):
 
 
 def makelimitorderiflowrsi(timeframeofdata, symbol, datalimit, amount, lowerthanrsi, ordergoodfor, profitpercentage):
+
+    #rsivalueString = (pullandprocess.returnLatestRsi(timeframeofdata, symbol, datalimit))
+    #rsivalue = float(rsivalueString)
+    print("attempting to buy {} {} stock, and gain at least {} percent, rsi value is ".format(amount, symbol, profitpercentage))
+
     print("attempting to buy {} {} stock, and gain at least {} percent".format(amount, symbol, profitpercentage))
+
     if float(pullandprocess.returnLatestRsi(timeframeofdata, symbol, datalimit).strip("\n")) < float(lowerthanrsi):
         price = pullandprocess.returnClosingVal(timeframeofdata, symbol, datalimit).strip("\n")
         #print(price)
@@ -51,31 +57,7 @@ def makelimitorderiflowrsi(timeframeofdata, symbol, datalimit, amount, lowerthan
 
 
 def makeLimitOrderIfRSIUp(timeframeofdata, symbol, datalimit, amount, lowerthanrsi, ordergoodfor, profitpercentage):
-    print("waiting 6 more minutes until resuming")
-    sleep(30)
-    print("waiting 5.5 more minutes until resuming")
-    sleep(30)
-    print("waiting 5 more minutes until resuming")
-    sleep(30)
-    print("waiting 4.5 more minutes until resuming")
-    sleep(30)
-    print("waiting 4 more minutes until resuming")
-    sleep(30)
-    print("waiting 3.5 more minutes until resuming")
-    sleep(30)
-    print("waiting 3 more minutes until resuming")
-    sleep(30)
-    print("waiting 2.5 more minutes until resuming")
-    sleep(30)
-    print("waiting 2 more minutes until resuming")
-    sleep(30)
-    print("waiting 1.5 more minutes until resuming")
-    sleep(30)
-    print("waiting 1 more minutes until resuming")
-    sleep(30)
-    print("waiting .5 more minutes until resuming")
-    sleep(30)
-    print("resuming")
+    sleep(360)
     if float(pullandprocess.returnLatestRsi(timeframeofdata, symbol, datalimit).strip("\n")) > float(lowerthanrsi):
         print("RSI value has been met, selling at yesterdays close price")
         priceAtClose = pullandprocess.returnClosingVal(timeframeofdata, symbol, datalimit).strip("\n")
@@ -93,6 +75,8 @@ def makeLimitOrderIfRSIUp(timeframeofdata, symbol, datalimit, amount, lowerthanr
 while True:
     
 	# thing to run
-    makelimitorderiflowrsi('1Min', 'SPY', '300', '1', '35', 'day', '1.03')
+    makelimitorderiflowrsi('1D', 'SPY', '300', '1', '35', 'day', '1.03')
     
+    makelimitorderiflowrsi('1Min', 'SPY', '300', '1', '35', 'day', '1.016')
+    #sleep(30)
 
